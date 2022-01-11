b7f159412d400dd6547735ce2b25f106abebef37
liuhejun <liuhejun@zhuofansoft.com>
2022-01-11 14:06:30 +0800

    pom

--- src/main/java/com/practice/designmode/proxy/JDKProxyTest.java
+++ src/main/java/com/practice/designmode/proxy/JDKProxyTest.java
@@ -13,7 +13,7 @@ public class JDKProxyTest {
         Student zhangsan = new Student("张三");
 
         //创建一个与代理对象相关联的InvocationHandler
-        InvocationHandler stuHandler = new StuInvocationHandler<Student>(zhangsan);
+        InvocationHandler stuHandler = new StuInvocationHandler(zhangsan);
 
         //创建一个代理对象stuProxy来代理zhangsan，代理对象的每个执行方法都会替换执行Invocation中的invoke方法
         Person stuProxy = (Person) Proxy.newProxyInstance(Student.class.getClassLoader(), new Class<?>[]{Person.class}, stuHandler);
